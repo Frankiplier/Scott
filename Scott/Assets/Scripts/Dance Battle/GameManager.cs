696b83b0db639b5e2a484417f5e3d13c8794812b
Frankiplier <agatarucka@gmail.com>
2024-06-26 17:27:48 +0200

    Fixed dance battle manager

--- Scott/Assets/Scripts/Dance Battle/GameManager.cs
+++ Scott/Assets/Scripts/Dance Battle/GameManager.cs
@@ -10,26 +10,14 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject word;
 
+    float timeToDance;
+
     void Start()
     {
          instance = this;
          word.SetActive(true);
-    }
 
-    void Update()
-    {
-        if (!startPlaying)
-        {
-            if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Escape) && !Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                word.SetActive(false);
-
-                startPlaying = true;
-                beatScroller.hasStarted = true;
-
-                theMusic.Play();
-            }
-        }
+         StartCoroutine(DanceBattle());
     }
 
     public void NoteHit()
@@ -40,5 +28,17 @@ public class GameManager : MonoBehaviour
     public void NoteMissed()
     {
         Debug.Log("Missed");
+    }
+
+    IEnumerator DanceBattle() 
+    {
+        yield return new WaitForSeconds(3);
+
+        word.SetActive(false);
+
+        startPlaying = true;
+        beatScroller.hasStarted = true;
+
+        theMusic.Play();
     }
 }
