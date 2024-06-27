272d9c70ed41b849e5c45bb4dd282a163c317c71
Frankiplier <agatarucka@gmail.com>
2024-06-27 09:25:12 +0200

    BG change party

---
+++ Scott/Assets/Scripts/Persistence/BGChange.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGChange : MonoBehaviour
+{
+    [SerializeField] AngryContainer angry;
+    [SerializeField] SadContainer sad;
+    [SerializeField] JelousContainer jelous;
+
+    public Sprite day, night;
+
+    void Update()
+    {
+        if (sad.sadDance == true && jelous.jelousDance == true && angry.angryDance == true)
+        {
+            GetComponent<SpriteRenderer>().sprite = night;
+        }
+    }
+}
